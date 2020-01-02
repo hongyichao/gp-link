80b9602c3e32b6ad39e5ea8f17583247bf8c2b96
Chao Hongyi <lucaschao@outlook.com>
2020-01-02 23:03:06 +1100

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DoctorsComponent } from './doctors/doctors.component';
 import { DoctorComponent } from './doctor/doctor.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+
+const appRoutes: Routes = [
+  { path: 'doctors', component: DoctorsComponent },
+  { path: 'doctor/:id',      component: DoctorComponent } ,
+  { path: 'doctor',      component: DoctorComponent }  
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DoctorsComponent,
-    DoctorComponent
+    DoctorComponent,
+    AppHeaderComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     FormsModule
   ],
--- src/app/doctors/doctors.component.ts
+++ src/app/doctors/doctors.component.ts
@@ -9,10 +9,42 @@ export class DoctorsComponent implements OnInit
 {
 
   doctorFound:string = "";
+  doctors: Array<Doctor>;
 
-  constructor() { }
+  constructor() 
+  { 
+this.doctors = new Array<Doctor>();
+
+     let d1: Doctor = new Doctor();
+     d1.FirstName = "Hongyi";
+     d1.LastName = "Chao";
+     d1.Phone = "12232";
+     d1.Email = "lucashyc@gmail.com";
+      
+     this.doctors.push(d1);
+
+let d2: Doctor  = {
+  FirstName:"Lucas",
+  LastName:"Chao",
+  Email:"lucashyc@gmail.com",
+  Phone:"0404610535"
+    };
+
+     this.doctors.push(
+     d2);
+
+  }
 
   ngOnInit() {
   }
 
+}
+
+export class Doctor
+{
+  public FirstName: string;
+  public LastName: string;
+  public Email: string;
+  
+  public Phone: string ;
 }
